5697ab6c4362c60bd5365aa0df09622b58471fff
rivaldofez <rivaldofez@gmail.com>
2024-07-27 14:40:08 +0700

    [FEAT] create emergency item model and main repository

---
+++ app/src/main/java/dev/ricoferdian/resiliencez/prayitna/data/MainRepository.kt
@@ -0,0 +1,23 @@
+package dev.ricoferdian.resiliencez.prayitna.data
+
+import dev.ricoferdian.resiliencez.prayitna.data.model.EmergencyItemModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+class ComicRepository {
+    suspend fun getEmergencyList(): Flow<List<EmergencyItemModel>> {
+        val items = ArrayList<EmergencyItemModel>()
+        for (i in 1..10) {
+            items.add(
+                EmergencyItemModel(
+                    id = i.toString(),
+                    name = "Ambulance",
+                    imageUrl = "",
+                    phoneNumber = "+62 822 7788 9900"
+                )
+            )
+        }
+
+        return flowOf(items)
+    }
+}
---
+++ app/src/main/java/dev/ricoferdian/resiliencez/prayitna/data/model/EmergencyItemModel.kt
@@ -0,0 +1,8 @@
+package dev.ricoferdian.resiliencez.prayitna.data.model
+
+data class EmergencyItemModel(
+    val id: String,
+    val name: String,
+    val imageUrl: String,
+    val phoneNumber: String
+)

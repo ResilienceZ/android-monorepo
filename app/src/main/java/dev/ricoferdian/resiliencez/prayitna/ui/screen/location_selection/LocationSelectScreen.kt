f341280a14a2c6a22a8833f30010ab52a0016a74
rivaldofez <rivaldofez@gmail.com>
2024-07-28 02:32:58 +0700

    [FEAT] implement reverse address location selection

--- app/src/main/java/dev/ricoferdian/resiliencez/prayitna/ui/screen/location_selection/LocationSelectScreen.kt
+++ app/src/main/java/dev/ricoferdian/resiliencez/prayitna/ui/screen/location_selection/LocationSelectScreen.kt
@@ -1,60 +1,72 @@
+@file:Suppress("DEPRECATION")
+
 package dev.ricoferdian.resiliencez.prayitna.ui.screen.location_selection
 
+import android.annotation.SuppressLint
 import android.preference.PreferenceManager
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
+import dev.ricoferdian.resiliencez.prayitna.ui.theme.CustomColor
 import dev.ricoferdian.resiliencez.prayitna.ui.theme.PrayitnaTheme
 import org.osmdroid.config.Configuration
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
 
+@SuppressLint("ClickableViewAccessibility")
 @Composable
 fun LocationSelectScreen(
     modifier: Modifier = Modifier,
     viewModel: LocationSelectViewModel = hiltViewModel()
 ) {
     val context = LocalContext.current
-    val coroutineScope = rememberCoroutineScope()
-    var address by remember { mutableStateOf("") }
-//    val nominatimService = remember { NominatimService.create() }
     var mapView by remember { mutableStateOf<MapView?>(null) }
     var mapMarker by remember { mutableStateOf<Marker?>(null) }
 
     val loadingValue = viewModel.loadingState.collectAsState()
     val reverseAddress by viewModel.addressState.collectAsState()
 
-    Column(modifier = Modifier.fillMaxSize()) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(CustomColor.White),
+        contentAlignment = Alignment.BottomCenter
+    ) {
         AndroidView(
             factory = {
-                Configuration.getInstance().load(context, PreferenceManager.getDefaultSharedPreferences(context))
+                Configuration.getInstance()
+                    .load(context, PreferenceManager.getDefaultSharedPreferences(context))
                 MapView(context).apply {
                     setMultiTouchControls(true)
                     controller.setZoom(15.0)
                     controller.setCenter(GeoPoint(-6.200000, 106.816666)) // Center on Jakarta
-
                     mapView = this
-
                     setOnTouchListener { _, event ->
                         if (event.action == android.view.MotionEvent.ACTION_UP) {
-                            val geoPoint = projection.fromPixels(event.x.toInt(), event.y.toInt()) as GeoPoint
+                            val geoPoint =
+                                projection.fromPixels(event.x.toInt(), event.y.toInt()) as GeoPoint
                             mapMarker?.let { overlays.remove(it) }
                             val marker = Marker(this).apply {
                                 position = geoPoint
@@ -62,23 +74,44 @@ fun LocationSelectScreen(
                             }
                             overlays.add(marker)
                             mapMarker = marker
-                                viewModel.getEvacuationMapList(geoPoint.latitude, geoPoint.longitude)
-
+                            viewModel.getEvacuationMapList(geoPoint.latitude, geoPoint.longitude)
                         }
                         false
                     }
                 }
             },
-            modifier = Modifier.weight(1f)
+            modifier = Modifier.fillMaxSize()
         )
 
-        Text(
-            text = reverseAddress?.display_name ?: "Got Null",
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
+                .background(CustomColor.White)
                 .padding(16.dp),
-            style = MaterialTheme.typography.bodyMedium
-        )
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = reverseAddress?.display_name ?: "Set pin point to generate address",
+                fontSize = 14.sp,
+                color = CustomColor.Black,
+                modifier = Modifier.weight(1f)
+            )
+
+            Box(
+                modifier = Modifier
+                    .background(CustomColor.DarkTangerin, shape = RoundedCornerShape(8.dp))
+                    .padding(4.dp)
+                    .clickable {  }
+
+            ){
+                Text(
+                    text = "Submit",
+                    fontSize = 16.sp,
+                    color = CustomColor.White,
+                )
+            }
+        }
     }
 }
 
